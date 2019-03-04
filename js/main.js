453f5f2f6ee5b5c05a18af91aec1f47ac76368d6
Vladimir Bazovic <vbazovic@gmail.com>
2019-03-04 14:02:07 +0100

    ispis prognoze test (sve u jedan prozor)

--- js/main.js
+++ js/main.js
@@ -10,9 +10,24 @@ function ucitaj() {
     .then(function (request) {
         return request.json();
     })
-      .then(result => {
-        console.log(result);
-        d('ispis').innerText = `Vremenska prognoza za ${result.name} je ${(result.main.temp-273.15).toFixed(1)} stepeni.`
+      .then(result => {        
+        let city = result.city;
+        let forecast = result.list;
+        let weather;
+        let dt;        
+        console.log(typeof forecast);
+        d('ispis').innerHTML= `${city.name}<br>`;        
+        forecast.forEach(function(element) {
+            dt = new Date(element.dt*1000); // epoch to date
+            console.log(element.weather);
+            // Iz dokumentacije It is possible to meet more than one weather condition for a requested location. The first weather condition in API respond is primary.
+            weather = element.weather[0]; // primary weather vidi https://openweathermap.org/weather-conditions
+            d('ispis').innerHTML += `Vreme ${dt.toLocaleString("sr")} <br/> stepeni ${element.main.temp}  <br/>`
+            d('ispis').innerHTML += `<img src="http://openweathermap.org/img/w/${weather.icon}.png" alt="${weather.description}"/><hr/>`            
+        });
+
+        
+
        
       })
   }
